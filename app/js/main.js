965952837d4291a9176ad0a966e2780e403fc2f4
Peresadchenko <1pers1@bk.ru>
2021-06-22 11:59:25 +0300

    added pagination

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function(){
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.1,
+    onStart: function (data) {  
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);      
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  })
+
+  $(".star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
